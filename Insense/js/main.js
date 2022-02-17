274d805f4c1aca03ca416f35b57fda3a90784954
hanna <kkokko5080@google.com>
2022-02-18 00:45:04 +0900

    insense

--- Insense/js/main.js
+++ Insense/js/main.js
@@ -1,11 +1,17 @@
-const img = document.querySelector('#img');
-const h2 = document.querySelector('#text h2');
-const p = document.querySelector('#text p');
-
 function opacityTransition(target, opacity, transition) {
   target.style.opacity = opacity;
   target.style.transition = transition;
 };
+function menuSlider(target, width, transition) {
+  target.style.width = width;
+  target.style.transition = transition;
+};
+
+
+const img = document.querySelector('#img');
+const h2 = document.querySelector('#text h2');
+const p = document.querySelector('#text p');
+
 window.addEventListener('load', function() {
   opacityTransition(img, '1', '2s');
   p.style.left = '0';
@@ -14,14 +20,11 @@ window.addEventListener('load', function() {
   opacityTransition(h2, '1', '2s');
 });
 
+
 // 마우스 오버시 메뉴가 나타남
 const meun = document.querySelector('#meun-botton');
 const nav = document.querySelector('nav');
 
-function menuSlider(target, width, transition) {
-  target.style.width = width;
-  target.style.transition = transition;
-};
 meun.addEventListener('mouseover', function() {
   menuSlider(nav, '22vw', '.5s');
 });
