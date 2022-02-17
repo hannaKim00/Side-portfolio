01da04c5f471ae81a29d0f611801dc028a6358ac
hanna <97264596+hannaKim00@users.noreply.github.com>
2022-02-17 16:48:50 +0900

    Insense

--- Insense/js/main.js
+++ Insense/js/main.js
@@ -1,13 +1,30 @@
+const img = document.querySelector('#img');
+const h2 = document.querySelector('#text h2');
+const p = document.querySelector('#text p');
+
+function opacityTransition(target, opacity, transition) {
+  target.style.opacity = opacity;
+  target.style.transition = transition;
+};
+window.addEventListener('load', function() {
+  opacityTransition(img, '1', '2s');
+  p.style.left = '0';
+  opacityTransition(p, '1', '2s');
+  h2.style.top = '0';
+  opacityTransition(h2, '1', '2s');
+});
+
+// 마우스 오버시 메뉴가 나타남
 const meun = document.querySelector('#meun-botton');
-// const nev = document.querySelector('nav');
-// 한나씨 nev 이거 오타임?
 const nav = document.querySelector('nav');
 
+function menuSlider(target, width, transition) {
+  target.style.width = width;
+  target.style.transition = transition;
+};
 meun.addEventListener('mouseover', function() {
-  nav.style.width = '22vw';
-  nav.style.transition = '.5s';
+  menuSlider(nav, '22vw', '.5s');
 });
 meun.addEventListener('mouseout', function() {
-  nav.style.width = '0vw';
-  nav.style.transition = '.5s';
+  menuSlider(nav, '0vw', '.5s');
 });
