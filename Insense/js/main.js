c5bd85ed3f308f22c8e05414a9ba3ad4cc744fee
kongukjae <kongukjae@gmail.com>
2022-02-17 15:12:39 +0900

    한나씨 오타인것 같아서 수정했음

--- Insense/js/main.js
+++ Insense/js/main.js
@@ -1,11 +1,13 @@
 const meun = document.querySelector('#meun-botton');
-const nev = document.querySelector('nav');
+// const nev = document.querySelector('nav');
+// 한나씨 nev 이거 오타임?
+const nav = document.querySelector('nav');
 
 meun.addEventListener('mouseover', function() {
-  nev.style.width = '22vw';
-  nev.style.transition = '.5s';
+  nav.style.width = '22vw';
+  nav.style.transition = '.5s';
 });
 meun.addEventListener('mouseout', function() {
-  nev.style.width = '0vw';
-  nev.style.transition = '.5s';
+  nav.style.width = '0vw';
+  nav.style.transition = '.5s';
 });
